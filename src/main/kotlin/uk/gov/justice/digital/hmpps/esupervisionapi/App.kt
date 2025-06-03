140b40e746b3f27ac94f388fec820e1f30a44ff7
Roland Sadowski <szabla@gmail.com>
2025-06-03 17:04:45 +0100

    rename the main app file/class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/App.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/App.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class EsupervisionApp
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<EsupervisionApp>(*args)
 }
