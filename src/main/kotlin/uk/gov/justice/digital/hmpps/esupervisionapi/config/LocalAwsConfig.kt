a64f4787c61c3cde07e1c7d9576965f2b60b8bc2
Roland Sadowski <szabla@gmail.com>
2025-06-17 13:50:42 +0100

    add LocalAwsConfig

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/config/LocalAwsConfig.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.esupervisionapi.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import java.net.URI
+
+@Configuration
+@Profile("dev")
+class LocalAwsConfig {
+  @Value("\${aws.region-name}")
+  lateinit var region: String
+
+  @Value("\${aws.endpoint-url}")
+  lateinit var endpointUrl: String
+
+  @Bean
+  fun s3Client(): S3Client {
+    LOG.info("Creating S3 client endpoint={}", endpointUrl)
+    return S3Client.builder()
+      .endpointOverride(URI.create(endpointUrl))
+      .region(Region.of(region))
+      .forcePathStyle(true)
+      .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("any", "any")))
+      .build()
+  }
+
+  companion object {
+    val LOG = LoggerFactory.getLogger(this::class.java)
+  }
+}
