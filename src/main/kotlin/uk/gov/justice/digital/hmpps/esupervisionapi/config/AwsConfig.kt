042e5dc5195ef42cb253e1a2a9a74394d70405d1
Roland Sadowski <szabla@gmail.com>
2025-06-18 12:39:28 +0100

    Add application-local.yml and tagged classes with 'local'

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/config/AwsConfig.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.esupervisionapi.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+
+@Configuration
+@Profile("!test & !local")
+class AwsConfig {
+
+  @Value("\${aws.region-name}")
+  lateinit var region: String
+
+  @Bean
+  fun s3Client(): S3Client = S3Client.builder()
+    .region(Region.of(region))
+    .build()
+
+  @Bean
+  fun s3Presigner(): S3Presigner = S3Presigner.builder()
+    .region(Region.of(region))
+    .build()
+}
