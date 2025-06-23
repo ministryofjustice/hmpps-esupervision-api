115c8c81fcb5a25c2cb6fb554f5f6982df5c5042
Lee Kitching <lee.kitching@justice.gov.uk>
2025-06-23 21:04:17 +0100

    Add route to create practitioners
    Add POST route to /practitioners which creates a pracitioner
    with the given properties.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/practitioner/PractitionerResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/practitioner/PractitionerResource.kt
@@ -1,18 +1,46 @@
 package uk.gov.justice.digital.hmpps.esupervisionapi.practitioner
 
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.util.UUID
 
 @RestController
 @RequestMapping("/practitioners", produces = [APPLICATION_JSON_VALUE])
 class PractitionerResource(private val practitionerService: PractitionerService) {
+
+  @PreAuthorize("hasRole('ROLE_ESUPERVISION__ESUPERVISION_UI')")
+  @Tag(name = "practitioner")
+  @PostMapping
+  fun createPractitioner(@RequestBody @Valid createRequest: PractitionerDto): ResponseEntity<Object> {
+
+    // TODO: validate in a spring-y way
+
+    val practitioner = Practitioner(
+      uuid = createRequest.uuid,
+      firstName = createRequest.firstName,
+      lastName = createRequest.lastName,
+      email = createRequest.email,
+      phoneNumber = createRequest.phoneNumber,
+    )
+    practitionerService.createPractitioner(practitioner)
+
+    val practitionerUrl = ServletUriComponentsBuilder.fromCurrentRequestUri()
+      .path("/{uuid}")
+      .buildAndExpand(practitioner.uuid)
+      .toUri()
+
+    return ResponseEntity.created(practitionerUrl).build()
+  }
 
   @PreAuthorize("hasRole('ROLE_ESUPERVISION__ESUPERVISION_UI')")
   @Tag(name = "practitioner")
