9ddd9b0e1e478ac2700e0ac779b891c4a7387d5b
Roland Sadowski <szabla@gmail.com>
2025-06-23 16:17:58 +0100

    Checkin API (#12)
    Adds API edpoints for complete offender checking flow (see docs/http/full-checkin-flow.http).

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/offender/OffenderCheckinDto.kt
@@ -0,0 +1,51 @@
+package uk.gov.justice.digital.hmpps.esupervisionapi.offender
+
+import java.net.URL
+import java.time.Instant
+import java.util.UUID
+
+enum class CheckinStatus {
+  CREATED,
+  SUBMITTED,
+  REVIEWED,
+}
+
+/**
+ *
+ */
+enum class AutomatedIdVerificationResult {
+  MATCH,
+  NO_MATCH,
+}
+
+enum class ManualIdVerificationResult {
+  MATCH,
+  NO_MATCH,
+}
+
+data class OffenderCheckinDto(
+  val uuid: UUID,
+  val status: CheckinStatus,
+  val dueDate: Instant,
+  val offender: OffenderDto,
+  val submittedOn: Instant?,
+  val questions: String,
+  val answers: String?,
+  val createdBy: UUID,
+  val createdAt: Instant,
+  val reviewedBy: UUID?,
+  /**
+   * Will be set to pre-signed S3 URL
+   */
+  val videoUrl: URL?,
+  val autoIdCheck: AutomatedIdVerificationResult?,
+  val manualIdCheck: ManualIdVerificationResult?,
+)
+
+/**
+ * Holds data submitted as a checkin by the offender.
+ */
+data class OffenderCheckinSubmission(
+  val offender: UUID,
+  val answers: String,
+)
