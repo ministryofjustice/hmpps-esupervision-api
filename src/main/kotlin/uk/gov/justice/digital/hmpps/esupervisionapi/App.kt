169e37d93c598c3c1c4a922a745ec7e80b27f151
Roland Sadowski <szabla@gmail.com>
2025-06-27 15:39:07 +0100

    remove unused imports

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/App.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/App.kt
@@ -1,12 +1,7 @@
 package uk.gov.justice.digital.hmpps.esupervisionapi
 
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import uk.gov.justice.digital.hmpps.esupervisionapi.practitioner.Practitioner
-import uk.gov.justice.digital.hmpps.esupervisionapi.practitioner.PractitionerService
-import java.util.UUID
 
 @SpringBootApplication
 class EsupervisionApp
