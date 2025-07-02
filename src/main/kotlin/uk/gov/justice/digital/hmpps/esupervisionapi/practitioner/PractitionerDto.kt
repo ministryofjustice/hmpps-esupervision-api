0f41713b32a5320a1a30efea001e5ce43b51d34b
Roland Sadowski <szabla@gmail.com>
2025-07-02 11:40:21 +0100

    Exception handler cleanup

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/practitioner/PractitionerDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/practitioner/PractitionerDto.kt
@@ -1,6 +1,11 @@
 package uk.gov.justice.digital.hmpps.esupervisionapi.practitioner
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+
 data class PractitionerDto(
+  @Schema(description = "Practitioner's unique ID (this can be NDelius ID)", required = true)
+  @field:NotBlank
   val uuid: String,
   val firstName: String,
   val lastName: String,
