0a9d9d8632fcb5421242e2f8099a339b25468559
Roland Sadowski <szabla@gmail.com>
2025-07-02 09:00:04 +0100

    basic integration test for '/practitioners' resource (#27)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/practitioner/PractitionerResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/practitioner/PractitionerResource.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.esupervisionapi.practitioner
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import uk.gov.justice.digital.hmpps.esupervisionapi.utils.BadArgumentException
 
 @RestController
 @RequestMapping("/practitioners", produces = [APPLICATION_JSON_VALUE])
@@ -29,7 +31,11 @@ class PractitionerResource(private val practitionerService: PractitionerService)
       email = createRequest.email,
       phoneNumber = createRequest.phoneNumber,
     )
-    practitionerService.createPractitioner(practitioner)
+    try {
+      practitionerService.createPractitioner(practitioner)
+    } catch (e: DataIntegrityViolationException) {
+      throw BadArgumentException("entity with given uuid already exists")
+    }
 
     val practitionerUrl = ServletUriComponentsBuilder.fromCurrentRequestUri()
       .path("/{uuid}")
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/PractitionerSetupTest.kt
@@ -0,0 +1,59 @@
+package uk.gov.justice.digital.hmpps.esupervisionapi.integration
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.esupervisionapi.practitioner.PractitionerDto
+
+class PractitionerSetupTest : IntegrationTestBase() {
+
+  private lateinit var practitionerCreatorRoleAuthHeaders: (HttpHeaders) -> Unit
+
+  @BeforeEach
+  internal fun setUp() {
+    practitionerCreatorRoleAuthHeaders = setAuthorisation(roles = listOf("ESUPERVISION__ESUPERVISION_UI"))
+  }
+
+  @Test
+  fun `create a a practitioner`() {
+    val newPractitioner = PractitionerDto(
+      java.util.UUID.randomUUID().toString(),
+      "Larry",
+      "Practitioner",
+      "larry@exmaple.com",
+      null,
+      roles = listOf(),
+    )
+
+    val createResult = createPractitionerRequest(newPractitioner)
+      .exchange()
+      .expectStatus().isCreated
+      .expectBody(Unit::class.java)
+      .returnResult()
+
+    val location = createResult.responseHeaders.getFirst(HttpHeaders.LOCATION)
+    Assertions.assertThat(location).isNotNull
+
+    val fetchResult = webTestClient.get()
+      .uri(location)
+      .headers(practitionerCreatorRoleAuthHeaders)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody(PractitionerDto::class.java)
+      .returnResult()
+
+    Assertions.assertThat(fetchResult.responseBody).isNotNull
+
+    createPractitionerRequest(newPractitioner)
+      .exchange().expectStatus().is4xxClientError
+  }
+
+  private fun createPractitionerRequest(newPractitioner: PractitionerDto): WebTestClient.RequestHeadersSpec<*> = webTestClient.post()
+    .uri("/practitioners")
+    .contentType(MediaType.APPLICATION_JSON)
+    .headers(practitionerCreatorRoleAuthHeaders)
+    .bodyValue(newPractitioner)
+}
