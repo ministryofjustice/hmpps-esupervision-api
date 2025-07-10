44b83d3d96fd1f415dfa5f909b4b98c38174de30
Roland Sadowski <szabla@gmail.com>
2025-07-10 14:50:26 +0100

    OffenderResource and OffenderCheckinResource accept pagination params (#35)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/offender/OffenderResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/offender/OffenderResource.kt
@@ -1,12 +1,15 @@
 package uk.gov.justice.digital.hmpps.esupervisionapi.offender
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.constraints.Max
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.esupervisionapi.utils.CollectionDto
 import uk.gov.justice.digital.hmpps.esupervisionapi.utils.toPagination
@@ -21,8 +24,12 @@ class OffenderResource(
   @Tag(name = "practitioner")
   @Operation(summary = "Returns a collection of offender records")
   @GetMapping
-  fun getOffenders(): ResponseEntity<CollectionDto<OffenderDto>> {
-    val pageRequest = PageRequest.of(0, 20)
+  fun getOffenders(
+    @Parameter(description = "Zero-based page index")
+    @RequestParam(defaultValue = "0") page: Int,
+    @RequestParam(defaultValue = "20") @Max(100) size: Int,
+  ): ResponseEntity<CollectionDto<OffenderDto>> {
+    val pageRequest = PageRequest.of(page, size)
     val offenders = offenderService.getOffenders(pageable = pageRequest)
     return ResponseEntity.ok(
       CollectionDto(
