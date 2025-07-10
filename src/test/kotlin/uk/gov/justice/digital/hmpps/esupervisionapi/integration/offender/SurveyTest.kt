717b7619b867ac87f0deed97e538b7b80419c8c1
Roland Sadowski <szabla@gmail.com>
2025-07-10 17:47:31 +0100

    flag survey responses
    The flagged responses are exposed on the checkin DTO instances
    as a computed property: `flaggedResponses`.
    
    From now on we will expect survey responses to contain a "version"
    entry. Function that implements what to flag/not flag will need to
    be optionally added to a mapping `versionToFlaggingFn`. If no such
    function is found (or no version string is found in the survey)
    `flaggedResponses` will be an empty list.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/offender/SurveyTest.kt
@@ -0,0 +1,97 @@
+package uk.gov.justice.digital.hmpps.esupervisionapi.integration.offender
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.esupervisionapi.offender.AutomatedIdVerificationResult
+import uk.gov.justice.digital.hmpps.esupervisionapi.offender.CheckinStatus
+import uk.gov.justice.digital.hmpps.esupervisionapi.offender.OffenderCheckinDto
+import uk.gov.justice.digital.hmpps.esupervisionapi.offender.OffenderDto
+import uk.gov.justice.digital.hmpps.esupervisionapi.offender.OffenderStatus
+import uk.gov.justice.digital.hmpps.esupervisionapi.offender.SurveyContents
+import java.time.Instant
+import java.time.LocalDate
+import java.util.UUID
+
+class SurveyTest {
+
+  val offender = OffenderDto(
+    UUID.randomUUID(),
+    "Bob",
+    "Bronson",
+    LocalDate.of(1990, 1, 1),
+    OffenderStatus.VERIFIED,
+    createdAt = Instant.now(),
+    "bob@example.com",
+    photoUrl = null,
+  )
+
+  val checkinTemplate = OffenderCheckinDto(
+    UUID.randomUUID(),
+    status = CheckinStatus.SUBMITTED,
+    dueDate = Instant.now(),
+    offender = offender,
+    submittedOn = Instant.now(),
+    surveyResponse = mapOf(),
+    createdBy = "alice",
+    createdAt = Instant.now(),
+    reviewedBy = null,
+    videoUrl = null,
+    autoIdCheck = AutomatedIdVerificationResult.MATCH,
+    manualIdCheck = null,
+  )
+
+  @Test
+  fun `get flagged fields`() {
+    val survey = makeSurvey(SurveyVersion.V20250710pilot)
+    val checkin = checkinTemplate.copy(surveyResponse = survey)
+
+    val flagged = checkin.flaggedResponses.toSet()
+    Assertions.assertEquals(setOf<String>(), flagged)
+
+    val updatedSurvey = survey.toMutableMap()
+
+    updatedSurvey.put("mentalHealth", "STRUGGLING" as Object)
+    Assertions.assertEquals(
+      setOf("mentalHealth"),
+      checkin.copy(surveyResponse = updatedSurvey.toMap()).flaggedResponses.toSet(),
+    )
+
+    updatedSurvey.put("assistance", listOf("HOUSING", "DRUGS") as Object)
+    Assertions.assertEquals(
+      setOf("mentalHealth", "assistance"),
+      checkin.copy(surveyResponse = updatedSurvey.toMap()).flaggedResponses.toSet(),
+    )
+  }
+
+  @Test
+  fun `get flagged fields when no version is present`() {
+    val checkin = checkinTemplate.copy(surveyResponse = mapOf())
+    Assertions.assertEquals(
+      listOf<String>(),
+      checkin.flaggedResponses,
+    )
+
+    Assertions.assertEquals(
+      listOf<String>(),
+      checkin.copy(surveyResponse = mapOf("callback" to "YES" as Object)).flaggedResponses,
+    )
+  }
+}
+
+enum class SurveyVersion(val version: String) {
+  V20250710pilot("2025-07-10@pilot"),
+}
+
+fun makeSurvey(version: SurveyVersion): SurveyContents {
+  when (version) {
+    SurveyVersion.V20250710pilot -> {
+      val survey = mapOf(
+        "version" to version.version as Object,
+        "mentalHealth" to "WELL" as Object,
+        "assistance" to listOf("NO_HELP") as Object,
+        "callback" to "NO" as Object,
+      )
+      return survey
+    }
+  }
+}
