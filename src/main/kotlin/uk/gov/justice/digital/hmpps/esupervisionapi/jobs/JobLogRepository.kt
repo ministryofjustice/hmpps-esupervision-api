f8fb2e711e15c6ec7bd27b9d6e4f47a0f4318b2c
Roland Sadowski <roland.sadowski@justice.gov.uk>
2025-08-13 11:13:59 +0100

    ensure the query only considers finished jobs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/jobs/JobLogRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/jobs/JobLogRepository.kt
@@ -1,10 +1,18 @@
 package uk.gov.justice.digital.hmpps.esupervisionapi.jobs
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.lang.Long
 import java.time.Instant
 
 @Repository
 interface JobLogRepository : org.springframework.data.jpa.repository.JpaRepository<JobLog, Long> {
+  @Query(
+    """
+    SELECT j FROM JobLog j 
+    WHERE j.createdAt >= :createdAt AND j.jobType = :jobType AND j.endedAt IS NOT NULL 
+    ORDER BY j.createdAt ASC
+  """,
+  )
   fun findByCreatedAtGreaterThanAndJobType(createdAt: Instant, jobType: JobType): List<JobLog>
 }
