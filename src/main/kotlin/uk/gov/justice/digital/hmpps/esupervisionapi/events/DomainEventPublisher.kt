40aad282ac16da42516ff686fe3cd5635bee2a79
Roland Sadowski <roland.sadowski@justice.gov.uk>
2025-09-08 12:09:05 +0100

    log publisher response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/events/DomainEventPublisher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/events/DomainEventPublisher.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.esupervisionapi.events
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.publish
@@ -14,9 +15,14 @@ class DomainEventPublisher(
     hmppsQueueService.findByTopicId("hmppseventtopic") ?: throw IllegalStateException("hmppseventtopic not found")
   }
   fun publish(domainEvent: DomainEvent) {
-    domainEventsTopic.publish(
+    val response = domainEventsTopic.publish(
       domainEvent.eventType,
       objectMapper.writeValueAsString(domainEvent),
     )
+    LOG.info("Published event to outbound topic, messageId={}, response={}", response.messageId(), response)
+  }
+
+  companion object {
+    val LOG: Logger = org.slf4j.LoggerFactory.getLogger(DomainEventPublisher::class.java)
   }
 }
