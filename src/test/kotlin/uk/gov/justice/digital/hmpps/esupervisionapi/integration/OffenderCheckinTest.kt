85a73c327ca9962ae1ca6872dfbdc979534f3945
Roland Sadowski <roland.sadowski@justice.gov.uk>
2025-09-11 17:24:26 +0100

    merge fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/OffenderCheckinTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/OffenderCheckinTest.kt
@@ -20,9 +20,9 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import uk.gov.justice.digital.hmpps.esupervisionapi.events.DomainEventPublisher
 import uk.gov.justice.digital.hmpps.esupervisionapi.notifications.NotificationService
-import uk.gov.justice.digital.hmpps.esupervisionapi.notifications.OffenderCheckinsStoppedMessage
 import uk.gov.justice.digital.hmpps.esupervisionapi.notifications.OffenderCheckinInviteMessage
 import uk.gov.justice.digital.hmpps.esupervisionapi.notifications.OffenderCheckinSubmittedMessage
+import uk.gov.justice.digital.hmpps.esupervisionapi.notifications.OffenderCheckinsStoppedMessage
 import uk.gov.justice.digital.hmpps.esupervisionapi.notifications.PractitionerCheckinSubmittedMessage
 import uk.gov.justice.digital.hmpps.esupervisionapi.offender.AutomatedIdVerificationResult
 import uk.gov.justice.digital.hmpps.esupervisionapi.offender.CheckinInterval
