08c2d889041b41f5b952849a08d7edb0dc147947
jameskingtpx <james.king@tpximpact.com>
2025-10-21 12:35:24 +0100

    Reordered imports

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/config/OpenApiConfiguration.kt
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
