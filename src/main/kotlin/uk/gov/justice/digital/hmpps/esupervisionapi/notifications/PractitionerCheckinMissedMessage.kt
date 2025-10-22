b504d0e6de99c1d70bcaf955a2a0cc85660d927e
Roland Sadowski <roland.sadowski@justice.gov.uk>
2025-10-22 13:55:25 +0100

    practitioner's expired checkin notif. should link to checkin view
    ...and not just to the dashboard

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/notifications/PractitionerCheckinMissedMessage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/notifications/PractitionerCheckinMissedMessage.kt
@@ -3,16 +3,18 @@ package uk.gov.justice.digital.hmpps.esupervisionapi.notifications
 import uk.gov.justice.digital.hmpps.esupervisionapi.config.AppConfig
 import uk.gov.justice.digital.hmpps.esupervisionapi.offender.OffenderCheckin
 import uk.gov.justice.digital.hmpps.esupervisionapi.practitioner.Practitioner
+import java.util.UUID
 
 data class PractitionerCheckinMissedMessage(
   val practitionerName: String,
   val offenderFirstName: String,
   val offenderLastName: String,
+  val checkin: UUID,
 ) : Message {
   override fun personalisationData(appConfig: AppConfig): Map<String, String> = mapOf(
     "practitionerName" to practitionerName,
     "name" to "$offenderFirstName $offenderLastName",
-    "popDashboardUrl" to appConfig.dashboardUrl().toString(),
+    "popDashboardUrl" to appConfig.checkinDashboardUrl(checkin).toString(),
   )
 
   override val messageType: NotificationType
@@ -23,6 +25,7 @@ data class PractitionerCheckinMissedMessage(
       practitionerName = practitioner.name,
       offenderFirstName = checkin.offender.firstName,
       offenderLastName = checkin.offender.lastName,
+      checkin = checkin.uuid,
     )
   }
 }
