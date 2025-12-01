405473d48b00095abe20aea6f9a7a0cc2e2f806b
Maddie Williams <maddie.williams1@gmail.com>
2025-12-01 11:24:46 +0000

    Enable caching

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/App.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/App.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.esupervisionapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
 import java.time.Clock
