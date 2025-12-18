64888356bf0c4204623bef6d17a5053ed310c4db
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2025-12-18 16:49:47 +0000

    Update 18_add_contact_preference_to_offender.sql

--- src/main/resources/db/changelog/changesets/18_add_contact_preference_to_offender.sql
+++ src/main/resources/db/changelog/changesets/18_add_contact_preference_to_offender.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset dave.iles:17_add_contact_preference_to_offender-1  splitStatements:false
+--changeset dave.iles:18_add_contact_preference_to_offender-1  splitStatements:false
 CREATE TYPE contact_type_v2 AS ENUM ('PHONE','EMAIL');
 
 ALTER TABLE offender_v2
