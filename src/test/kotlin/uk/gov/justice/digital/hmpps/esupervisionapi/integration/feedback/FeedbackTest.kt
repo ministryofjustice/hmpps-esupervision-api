5472db8845b57473924849080be5ac4265cd95ba
Rob Catton <robmcatton@gmail.com>
2026-01-20 10:49:11 +0000

    Fixing typo

--- src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/feedback/FeedbackTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/feedback/FeedbackTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.esupervisionapi.integration.health
+package uk.gov.justice.digital.hmpps.esupervisionapi.integration.feedback
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
