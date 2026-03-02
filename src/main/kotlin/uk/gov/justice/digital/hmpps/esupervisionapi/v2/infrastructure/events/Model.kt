446314087daded5bcd2d8ee7276ec6cedc93aa0c
Roland Sadowski <roland.sadowski@justice.gov.uk>
2026-03-02 15:15:46 +0000

    fix: missing event definitions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/v2/infrastructure/events/Model.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/v2/infrastructure/events/Model.kt
@@ -2,12 +2,15 @@ package uk.gov.justice.digital.hmpps.esupervisionapi.v2.infrastructure.events
 
 import java.time.ZonedDateTime
 
+data class AdditionalInformation(val eventNumber: Long? = null)
+
 data class DomainEvent(
   val eventType: String,
   val detailUrl: String,
   val occurredAt: ZonedDateTime,
   val description: String,
   val personReference: PersonReference?,
+  val additionalInformation: AdditionalInformation? = null,
 )
 
 data class PersonReference(val identifiers: List<PersonIdentifier>) {
