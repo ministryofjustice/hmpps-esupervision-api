8fd6572ac6982dbaf392073b060e474fc1d969d0
Roland Sadowski <szabla@gmail.com>
2026-03-04 09:57:21 +0000

    ESUP-1239: publish proxied media links to NDelius (#214)
    Feature hidden behind a flag, disabled by default. Enabled for dev.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/utils/ProxyLinkCreator.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.esupervisionapi.utils
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.esupervisionapi.config.AppConfig
+import uk.gov.justice.digital.hmpps.esupervisionapi.v2.OffenderCheckinV2
+import uk.gov.justice.digital.hmpps.esupervisionapi.v2.OffenderV2
+import java.net.URI
+
+@Component
+class ProxyLinkCreator(
+  val appConfig: AppConfig,
+) {
+
+  fun checkinSnapshot(checkin: OffenderCheckinV2, index: Int = 0): URI = URI.create("${appConfig.mediaProxyUrl()}/checkin/${checkin.uuid}/snapshot?index=$index")
+
+  fun checkinVideo(checkin: OffenderCheckinV2): URI = URI.create("${appConfig.mediaProxyUrl()}/checkin/${checkin.uuid}/video")
+
+  fun offenderReferencePhoto(offender: OffenderV2): URI = URI.create("${appConfig.mediaProxyUrl()}/offender/${offender.uuid}/photo")
+}
