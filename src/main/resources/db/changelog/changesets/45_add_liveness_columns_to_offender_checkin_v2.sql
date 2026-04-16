4854056fce9a8fb446624990dfab4173c6729b44
Roland Sadowski <roland.sadowski@justice.gov.uk>
2026-04-16 10:17:43 +0100

    introduce postgres enum for liveness_result

--- src/main/resources/db/changelog/changesets/45_add_liveness_columns_to_offender_checkin_v2.sql
+++ src/main/resources/db/changelog/changesets/45_add_liveness_columns_to_offender_checkin_v2.sql
@@ -1,12 +1,20 @@
 --liquibase formatted sql
 
---changeset richard-birch:43_add_liveness_columns_to_offender_checkin_v2 splitStatements:false
+--changeset richard-birch:45_add_liveness_columns_to_offender_checkin_v2 splitStatements:false
+
+CREATE TYPE liveness_result AS ENUM (
+    'LIVE',
+    'NOT_LIVE',
+    'ERROR'
+);
+
 ALTER TABLE offender_checkin_v2
-    ADD COLUMN liveness_result VARCHAR(10),
+    ADD COLUMN liveness_result liveness_result,
     ADD COLUMN liveness_confidence REAL;
 
 ALTER TABLE event_audit_log_v2
-    ADD COLUMN liveness_result VARCHAR(10);
+    ADD COLUMN liveness_result liveness_result;
 
 --rollback ALTER TABLE offender_checkin_v2 DROP COLUMN liveness_result, DROP COLUMN liveness_confidence;
 --rollback ALTER TABLE event_audit_log_v2 DROP COLUMN liveness_result;
+--rollback DROP TYPE liveness_result;
