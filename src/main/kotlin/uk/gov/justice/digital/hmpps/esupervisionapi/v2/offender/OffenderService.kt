100b70b4aafb6dcf04a9b427f3c7aa8d0c93f455
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2026-08-19 12:19:25 +0100

    Use NOT_FOUND when no overalRisk

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/v2/offender/OffenderService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/v2/offender/OffenderService.kt
@@ -64,7 +64,7 @@ class OffenderService(
       dateOfBirth = contactDetails.dateOfBirth,
       tierScore = tierDetails.tierScore,
       tierDetailsLink = "$tierUiBaseUri/case/$crn",
-      overallRisk = arnsWidget.overallRisk ?: "NONE",
+      overallRisk = arnsWidget.overallRisk ?: "NOT_FOUND",
     )
   }
 
