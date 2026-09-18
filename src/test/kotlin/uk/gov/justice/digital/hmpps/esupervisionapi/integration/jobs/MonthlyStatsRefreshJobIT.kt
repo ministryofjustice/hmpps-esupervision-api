ca1609d95243d2b0a494d847bdbdb8f162deeeb6
Roland Sadowski <roland.sadowski@justice.gov.uk>
2026-09-18 09:43:17 +0100

    Revert "fix: test data band-aid"
    This reverts commit d4a34f6dfe00488a68ba416592b345577326c8ec.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/jobs/MonthlyStatsRefreshJobIT.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/esupervisionapi/integration/jobs/MonthlyStatsRefreshJobIT.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.esupervisionapi.integration.jobs
 
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
@@ -23,11 +22,6 @@ class MonthlyStatsRefreshJobIT : IntegrationTestBase() {
 
   private val fixedClock: Clock =
     Clock.fixed(Instant.parse("2026-02-22T12:00:00Z"), ZoneOffset.UTC)
-
-  @BeforeEach
-  fun setupDb() {
-    jdbcTemplate.update("TRUNCATE TABLE event_audit_log_v2 RESTART IDENTITY CASCADE")
-  }
 
   @AfterEach
   fun cleanDb() {
